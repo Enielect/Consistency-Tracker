8a78a64a4327dc83413a2d3ca097dcdb648bbfe8
eniola <eniolafaithful57@gmail.com>
2024-01-05 04:44:06 +0100

    initialized project

---
+++ src/components/Box.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// interface BoxProps {
+//   // Add your component props here
+// }
+
+const Box: React.FC = () => {
+  return (
+    <div className=''>
+
+    </div>
+  );
+};
+
+export default Box;
