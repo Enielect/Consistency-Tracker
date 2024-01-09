af5a205a31797ccda36c2162606f849f7c2536e2
eniola <eniolafaithful57@gmail.com>
2024-01-09 17:57:29 +0100

    improved the styling for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './index.css'
+import "./index.css";
 
 function App() {
-  return <div className=" text-3xl font-bold">what is the problem with you</div>;
+  return (
+    <div className="bg-white h-screen w-screen text-3xl font-bold">
+      what is the problem with you
+    </div>
+  );
 }
 
 export default App;
